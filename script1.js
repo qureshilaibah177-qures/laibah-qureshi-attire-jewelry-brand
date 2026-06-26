d4fbce62071fbd230805040d724897d7735323e8
LAIBA AMJAD QURESHI <qureshilaibah177@gmail.com>
2026-06-26 13:40:21 +0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,14 @@
+// Smooth Scroll for Navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+console.log("Laibah Qureshi's Attire loaded with 15 unique items.");
